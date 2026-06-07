4feb0e3fb6cde9a790f5f5ec4d0d60e6a236297e
brianvarskonst <burnify.design@gmail.com>
2026-06-08 01:13:46 +0200

    feat(event-dispatcher): add bundle service wiring

---
+++ src/EventDispatcherBundle.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SymPress\EventDispatcher;
+
+use SymPress\Kernel\Bundle\AbstractBundle;
+
+final class EventDispatcherBundle extends AbstractBundle
+{
+}
